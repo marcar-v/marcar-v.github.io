18198cc77628fb6a656c6f1ac0c5bf0e8ebf0f11
marcar-v <137494495+marcar-v@users.noreply.github.com>
2026-01-29 17:48:53 +0100

    change github

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Portfolio de Violeta";
+export const SITE_TITLE = "Portfolio de Violeta Martín";
 export const SITE_DESCRIPTION = "Mi portfolio";
