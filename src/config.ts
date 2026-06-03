c672956d9036025a1f08f51c02c25e2a6895399d
marcar-v <137494495+marcar-v@users.noreply.github.com>
2026-06-03 12:28:14 +0200

    english translation

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Portfolio de Violeta Martín";
-export const SITE_DESCRIPTION = "Mi portfolio";
+export const SITE_TITLE = "Violeta Martin Portfolio";
+export const SITE_DESCRIPTION = "My portfolio";
