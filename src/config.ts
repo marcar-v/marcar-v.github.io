6eaa9339e46b2265c716a9a1b820c2c3d4667659
marcar-v <137494495+marcar-v@users.noreply.github.com>
2026-06-03 13:04:19 +0200

    fix games description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Violeta Martin Portfolio";
-export const SITE_DESCRIPTION = "My portfolio";
+export const SITE_TITLE = "Violeta Martín | Game & Software Developer";
+export const SITE_DESCRIPTION = "Portfolio of a gameplay programmer and software developer focused on interactive systems, game mechanics, and prototyping";
